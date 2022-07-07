821085733c3fe3aa7663578027c9bf13e9b7e9aa
Ian Rigg <ianjrigg@gmail.com>
2022-07-06 20:27:50 -0500

    Add device number assignment

--- drivers/lcd-1602-driver.c
+++ drivers/lcd-1602-driver.c
@@ -5,13 +5,24 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
+
+#define DEVICE_NAME "LCD_1602"
+
+dev_t dev = 0;
 
 /**
  * LCD_1602_driver_init(void) - Init function for the 1602 driver
  * Return: 0 for normal execution, negative for anything abnormal
  */
 static int __init LCD_1602_driver_init(void)
-{  
+{
+        /* Dyanmically allocate the device number */
+        if(alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME) < 0) {
+                pr_info("Unable to allocate device number. Exiting...\n");
+                return -1;
+        }
+
         pr_info("1602 driver inserted.\n");
         return 0;
 }
@@ -22,6 +33,7 @@ static int __init LCD_1602_driver_init(void)
  */
 static void __exit LCD_1602_driver_exit(void)
 {
+        unregister_chrdev_region(dev, 1);
         pr_info("1602 driver removed.\n");
 }
  
